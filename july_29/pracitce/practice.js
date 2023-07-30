dcf66e3948854fb09a34ba43199dbff41c287bbd
devjunayed <devjunayed@gmail.com>
2023-07-30 23:14:55 +0600

    july 29 done

---
+++ july_29/pracitce/practice.js
@@ -0,0 +1,63 @@
+/** Practice Problem 1
+ * you are give an array;
+ * var fruits = ['Apple', 'Bananan', 'Orange'];
+ * 
+ * a) Find the index of 'Banana' and replace 'Banana' with  'Mango'.
+ * b) Remove 'Orange' and add 'Watermelon'.
+ */
+
+
+/** Pracitce Problem 2
+ * You and your friends Tom, Jane,Peter and John got their final exam
+ * results. your toal score is 85, Tom's total score is 66, janes' total
+ * score is 95, Peter's total score is 56 and John's total score is 40.
+ * The grading chart is 
+ * 
+ * 80 or above A grade
+ * 60 or above B grade
+ * 50 or above C grade
+ * 40 or above D grade
+ * 39 or less => F grade
+ * 
+ * Write a program to find your and your friend's grades usign if-else.
+ */
+
+/** Practice problem 3
+ * 1. You are give three numbers 13, 79, and 45. Write a program that will
+ * print the largest number using if-else.
+ * 
+ * 2. Your are give a triangle with the sides 9, 8, 9. Write a program to 
+ * check whether a triangle is Isosceles or not using if-else.
+ *              
+ *              Isosceles => two sides are equal
+ */
+
+
+
+/**
+ * প্রাকটিস চ্যালেঞ্জ -১:  
+ * ক্লাস সেভের এর ফাইনাল এক্সাম এ তুমি ফার্স্ট হয়েছ। তোমার বন্ধু আলিয়া, ডালিয়া, সালিয়া,  * মালিয়া, লিলিয়া আর জ্বালাইয়া - তাদের grade তুমি জানো না। তবে তাদের নম্বর জানো. আলিয়া  * 95 পেয়েছে, ডালিয়া 66 পেয়েছে, সালিয়া 80 পেয়েছে, মালিয়া পেয়েছে 59, লিলিয়া পেয়েছে 
+ * 47, জ্বালাইয়া পেয়েছে 77। তুমি JS code দিয়ে তাদের grade বের করে দিতে পারবে?
+ *
+ * ১) যারা ৫০ এর নিচে পেয়েছে, তাদের grade F.
+ * 
+ * ২) যারা ৫০ বা তার উপরে পেয়েছে, অথবা ৬০ এর নিচে পেয়েছে, তাদের grade D.
+ *
+ * ৩) যারা ৬০ বা তার উপরে পেয়েছে, অথবা ৭০ এর নিচে পেয়েছে, তাদের grade C.
+ *
+ * ৪) যারা ৭০ বা তার উপরে পেয়েছে, অথবা ৮০ এর নিচে পেয়েছে, তাদের grade B.
+ *
+ * ৫) যারা ৮০ বা তার উপরে পেয়েছে, অথবা ৯০ এর নিচে পেয়েছে, তাদের grade A.
+ * 
+ * ৬) যারা ৯০ বা তার উপরে পেয়েছে, তাদের grade A+.
+ */
+
+
+
+ /** প্রাকটিস প্রব্লেম-২: 
+ *
+ * তাড়াহুড়া করে স্কুলের জন্য বের হচ্ছ কিন্তু রাস্তা পার হওয়ার সময় দেখলে, ট্রাফিক  
+ * সিগন্যাল লাল রং। এই অবস্থায় যদি তুমি রাস্তা পার হওয়ার চেষ্টা কর, তাহলে ডেঞ্জার হতে  * পারে। যদি হলুদ রং হয় তাহলে তোমার থেমে যাওয়া উচিত। আর যদি ট্রাফিক সিগন্যাল গ্রিন হয়  * তাহলে তোমার রাস্তা পার হওয়া উচিত। তাই একটা কোড লিখে ফেলো। যেখানে আমরা signal নামে  
+ * একটা ভেরিয়েবল থাকবে। সেটার মান green, yellow বা red হতে পারে। সেই অনুসারে ডিফারেন্ট  * ডিফারেন্ট কাজ হবে। তো, সেই কোড ফটাফট লিখে ফেলো।
+ * 
+ */
