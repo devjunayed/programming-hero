ad7ae701c9d663ca029c61e451451b389555b4e6
devjunayed <devjunayed@gmail.com>
2023-07-30 14:43:20 +0600

    arrya

---
+++ july_29/learning/array.js
@@ -0,0 +1,6 @@
+var friendsAge = [11, 21, 45, 17, 14, 105, 6];
+var picnicFee = [5000, 2000, 4000, 150]; 
+
+var nayikas = ['mahi', 'opu', 'sabnoor', 'sabana'];
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
