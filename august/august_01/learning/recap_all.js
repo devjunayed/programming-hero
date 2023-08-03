b1804465c884f2979f5f182799c526bb9dfc4422
devjunayed <devjunayed@gmail.com>
2023-08-04 00:34:00 +0600

    recal-all.js

---
+++ august/august_01/learning/recap_all.js
@@ -0,0 +1,51 @@
+/*
+    ==============
+    || Variable ||
+    ==============
+    1. What is JavaScript?
+    2. How does JS Works?
+    3. What is variable?
+    4. Declare a variable
+    5. Types of variable? How can you find out type of a variable
+    6. Primitive and not-primitive data types
+    7. Naming convention of JS variables
+    8. Math Operation +, -, *, /, %
+    9. Shorthand: +=, -+, *=, /=
+    10. ++, --
+    11. parseInt, parseFloat
+    12. toFixed, toPrecision
+
+
+    
+    ===========
+    || Array ||
+    ===========
+    1. What is the purpose?
+    2. How to declare an array in JS
+    3. Number of elements in an Array
+    4. What is index?
+    5. Find the value of an element by index
+    6. Change an element by index
+    7. get the index of an element by the value
+    8. what does it mean when you get undefined by while getting
+       the value of an element
+    9. How can you add an element to an array at the last position
+    10. How can you remove the last element from an array
+    11. Add an element at the first position of an array
+    12. Remove the first element of an array
+     
+
+
+    ==================
+    || Conditionals ||
+    ==================
+    1. meaning of: >, <, >=, <=, ==, !=, ===, !===
+    2. Meaning of &&
+    3. Meaning of ||
+    4. Let's say you have x amount of mone. if you have more
+       than 80k then you will buy a mac. if more than 60k then
+       you will buy gaming latptop. if you have more than 40k 
+       then you will buy lenoveo Yoga. if you have more than 20k
+       then you will by an used laptop otherwise, you will use 
+       your mobile phone
+ */
