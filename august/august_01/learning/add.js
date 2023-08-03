8033d73fefa189c9f72e4710e2d3c7913725fdb9
devjunayed <devjunayed@gmail.com>
2023-08-03 21:57:00 +0600

    august 01

---
+++ august/august_01/learning/add.js
@@ -0,0 +1,10 @@
+function add(num1, num2){
+    const sum = num1 + num2;
+    return sum;
+}
+
+const result1 = add(12, 13);
+const result2 = add(35, 7);
+const finalResult = add(result1, result2);
+
+console.log(finalResult); 
