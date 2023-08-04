06587bef3231bf1feebfd26c9c82fffb7f777422
devjunayed <devjunayed@gmail.com>
2023-08-05 00:07:38 +0600

    august 02 half done

--- august/august_02/learning/findOddSum.js
+++ august/august_02/learning/findOddSum.js
@@ -7,6 +7,7 @@ and will return the sum of the odd numbers.
 
 */ 
 
+// sum of an array
 function getSumOfAnArray(numbers){
     let sum = 0;
     for(let i = 0; i < numbers.length; i++){
@@ -15,6 +16,22 @@ function getSumOfAnArray(numbers){
     return sum;
 }
 
+// sum of odd in an array
+function getOddNumbersOfAnArray(numbers){
+    let sumOfOdd = 0;
+    for(let i = 0; i < numbers.length; i++){
+        if(numbers[i] % 2 !== 0){
+            sumOfOdd += numbers[i];
+        }
+    }
+    return sumOfOdd;
+}
+
+
 const myNumbers = [5, 7, 8, 10, 45, 30];
 const sumOfArray = getSumOfAnArray(myNumbers);
+const oddSumOfArray = getOddNumbersOfAnArray(myNumbers);
+
+
 console.log(sumOfArray);
+console.log(oddSumOfArray);
