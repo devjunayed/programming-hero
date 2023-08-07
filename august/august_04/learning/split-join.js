26a7ecd48547acfc2dbd969fb992ade5de9fe7a9
devjunayed <devjunayed@gmail.com>
2023-08-07 11:02:55 +0600

    split, slice, substring, trim, join, indexOf

--- august/august_04/learning/split-join.js
+++ august/august_04/learning/split-join.js
@@ -8,3 +8,24 @@ const sentences = lyrics.split('.');
 
 const chars = lyrics.split('');
 // console.log(chars);
+
+const partial1 = lyrics.slice(5, 8);
+// console.log(partial1);
+
+const partial2 = lyrics.substring(5, 8);
+// console.log(partial2);
+
+
+const greeting = '     Hello World        ';
+// console.log(greeting.trim());
+
+
+const lines = [
+    'Tumi bondhu kala pakhi ami jeno ki',
+    'bosonto kala tomai bolte pari ni',
+    'kala kala sadha sadha',
+    'rong jomeche sadha kala'
+  ];
+
+  const newSong = lines.join(". ");
+  console.log(newSong);
