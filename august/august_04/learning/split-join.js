0669335f03d9f919d460eb0bd8e80d207631a833
devjunayed <devjunayed@gmail.com>
2023-08-07 10:44:59 +0600

    split-join.js

---
+++ august/august_04/learning/split-join.js
@@ -0,0 +1,10 @@
+const lyrics = 'Tumi bondhu kala pakhi ami jeno ki. bosonto kale tomai bolte pari ni. kala kala sadha sadha';
+
+const parts = lyrics.split(' ');
+// console.log(parts);
+
+const sentences = lyrics.split('.');
+// console.log(sentences);
+
+const chars = lyrics.split('');
+// console.log(chars);
