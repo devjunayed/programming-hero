51ec38e85b1a0a995162ff18cc331bfc4b09f2b7
devjunayed <devjunayed@gmail.com>
2023-08-19 10:24:09 +0600

    addEventListener

--- august/august_12/learning/js/event.js
+++ august/august_12/learning/js/event.js
@@ -19,3 +19,25 @@ const purpleButton = document.getElementById('make-purple');
 purpleButton.onclick = function(){
   document.body.style.backgroundColor = 'purple';
 }
+
+// option: 4
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink);
+
+function makePink(){
+    document.body.style.backgroundColor = 'pink';
+}
+
+// option: 4 another
+const makeGrenButton = document.getElementById('make-green');
+makeGrenButton.addEventListener('click', function makeGreen(){
+ document.body.style.backgroundColor = 'green';
+});
+
+
+// option: 4 final
+document
+.getElementById("make-goldenrod")
+.addEventListener("click", function () {
+  document.body.style.backgroundColor = "goldenrod";
+});
