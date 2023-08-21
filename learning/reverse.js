1475ae20b0ee06af6467e034f6dd7231f0aaa09d
devjunayed <devjunayed@gmail.com>
2023-08-21 12:02:04 +0600

    repushing

---
+++ learning/reverse.js
@@ -0,0 +1,25 @@
+/* var number = 0;
+while(number < 10){
+    console.log(number);
+    number++;
+} */
+
+
+// reverse
+/* var num = 10;
+
+while(num >= 1){
+    console.log(num);
+    num--;
+} */
+
+
+/* for(var i = 1; i <=10; i++){
+    console.log(i);
+} */
+
+// reverse for loop
+
+for(var i = 10; i >= 1; i--){
+    console.log(i);
+}
---
+++ practice/practice_task_6.js
@@ -0,0 +1,4 @@
+/**
+ * 6. Declare an array for all the topics that you have learned
+ * last few days and display the array
+ */
