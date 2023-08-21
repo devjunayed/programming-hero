5b389a7239cc38bf8f1da868fdc0346fa87a53cd
devjunayed <devjunayed@gmail.com>
2023-08-21 12:00:06 +0600

    repushing

---
+++ learning/variable-name.js
@@ -0,0 +1,35 @@
+// good variables
+var price = 29;
+
+//  1. variable name can not be any keywords
+var false = 96; //invalid
+var return = true; //invalid
+
+// 2. variable name has to be in one word. No space
+var my home address = "New California"; //invalid
+
+// 3. variable name can not have quotation
+var "name" = "Tom Hanks"; //invalid
+
+
+// 4. variable name cannot starts with a number but can ends with a number
+var 99club = 1964; //invalid
+
+var club25 = 2025; //valid
+
+// 5. How to use long names
+// cannot use dash
+var user-name = "Raj Bappa"; //invalid
+
+var user_name = "bappa raj"; //valid
+var usercurrenthomeaddress = "andor killa bandor ban"; //valid but not good
+var user_current_home_address = "andor killa bandor ban"; //valid (snake case)
+var userCurrentHomeAddress = "andor killa bandor ban"; //valid  (camel case)
+var UserCurrentHomeAddress = "andor killa bandor ban"; //valid (pascal case)
+
+// variable name is case sensitive
+// person and Person are not same
+var person = 25; 
+var Person = 35;
+
+
