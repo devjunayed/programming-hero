7874eaf6566bcecbcf65fd51ebe605bc185ac9a9
devjunayed <devjunayed@gmail.com>
2023-08-21 12:11:50 +0600

    repushing

---
+++ learning/function.js
@@ -0,0 +1,11 @@
+// function declaration
+function startFan(){
+    console.log('Stand Up');
+    console.log('walk towards the switch');
+    console.log('Press the switch');
+}
+
+// call the function
+startFan();
+startFan();
+startFan();
