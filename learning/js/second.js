78c0944dfbb724c561d1ae70de8f02e3990b26f1
devjunayed <devjunayed@gmail.com>
2023-08-21 12:28:13 +0600

    repushing

--- learning/js/second.js
+++
@@ -1,8 +0,0 @@
-console.log("second js file")
-
-const secondList = document.getElementById('second-list');
-
-const li = document.createElement('li');
-li.innerText = 'My dynamic item';
-
-secondList.appendChild(li);
