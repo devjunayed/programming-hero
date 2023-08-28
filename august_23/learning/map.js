0427d621b1189255a0c28e900ee3060412874023
devjunayed <devjunayed@gmail.com>
2023-08-28 12:55:03 +0600

    taking a break

--- august_23/learning/map.js
+++ august_23/learning/map.js
@@ -1,36 +1,25 @@
-// map ==> loops through each element of the array and do the operation that you passed in the call back function and hold the result from each operation in an array and finally returns you the array
-const numbers = [4, 5, 2, 8,10];
-// const doubled = [];
+// map ==> loops through each element of the array and do the operation that you passed in the call back function and hlode the result from each operation in an array and finally returns you the array
 
-// function doubleIt(num){
-//     return num*2;
-// }
+const numbers = [4, 5, 2, 8, 10];
 
-// const result = numbers.map(doubleIt);
+function doubleIt(num){
+    // console.log('num now', num);
+    return num*2;
+}
+
+const double2 = n => n*2;
+
+const result = numbers.map(doubleIt);
 // console.log(result);
 
+const output = numbers.map(double2);
+// console.log(output);
+const output2 = numbers.map(n => n*2);
+console.log(output2);
+
+// const doubled = [];
 // for(const num of numbers){
-//     const double = num * 2;
+//     const double = num *2;
 //     doubled.push(double);
 // }
-
 // console.log(doubled);
-
-
-const doubled = numbers.map(num => num*2);
-// console.log(doubled);
-
-const fiveBonus = numbers.map(num => num + 5);
-// console.log(fiveBonus);
-
-const halves = numbers.map(num => num/2);
-// console.log(halves);
-
-
-const friends = ['Tom', 'John', 'Michel', 'Oliver'];
-
-const lengths = friends.map(frnd => frnd.length);
-// console.log(lengths);
-
-const firstLetter = friends.map(frnd => frnd[0]);
-console.log(firstLetter);
