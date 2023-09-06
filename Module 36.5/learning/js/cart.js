11e7be8c2cc4ac450346b441159170a36eadf281
devjunayed <devjunayed@gmail.com>
2023-09-06 15:18:55 +0600

    local storage

--- Module 36.5/learning/js/cart.js
+++ Module 36.5/learning/js/cart.js
@@ -3,7 +3,42 @@ const addProduct = () => {
     const productQuantity = document.getElementById('product-quantity');
     const product = productField.value;
     const quantity = productQuantity.value;
-    console.log(product, quantity);
     productField.value = "";
     productQuantity.value = "";
+
+    displayProduct(product, quantity);
+    saveProductToLocalStorage(product, quantity);
 }
+
+
+const displayProduct = (product, quantity) => {
+    const ul = document.getElementById('product-container');
+    const li = document.createElement('li');
+    li.innerText = `${product}: ${quantity}`;
+    ul.appendChild(li);
+}
+
+const getStroedShoppingCart = () => {
+    let cart = {};
+    const storedCart  = localStorage.getItem('cart');
+    if(storedCart){
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+const saveProductToLocalStorage = (product, quantity) => {
+    const cart = getStroedShoppingCart();
+     cart [product] =  quantity;
+     const cartStringified = JSON.stringify(cart);
+     localStorage.setItem('cart', cartStringified);
+    }
+    
+const displayProductsFromLocalStorage = () => {
+    const savedCart = getStroedShoppingCart();
+    for(const product in savedCart){
+        const quantity = savedCart[product];
+        displayProduct(product, quantity);
+    }
+}
+    displayProductsFromLocalStorage();
